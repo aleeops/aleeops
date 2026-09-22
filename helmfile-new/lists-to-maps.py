b4f8a1c5645dc4f3b12f01fe3cbcb8c4fe1512c7
Andy <alee68dmv@gmail.com>
2026-09-22 00:25:25 -0400

    Update Helmfile notes

--- helmfile-new/lists-to-maps.py
+++ helmfile-new/lists-to-maps.py
@@ -3,20 +3,39 @@ import argparse
 import yaml
 
 def list_to_map(data):
-    """Converts extraEnvs list to envMap dictionary."""
+    """Converts extraEnvs list to envMap dictionary, preserving complex structures like valueFrom."""
     if 'extraEnvs' in data:
         if isinstance(data['extraEnvs'], list):
-            data['envMap'] = {item['name']: item['value'] for item in data['extraEnvs']}
+            data['envMap'] = {}
+            for item in data['extraEnvs']:
+                name = item.get('name')
+                if not name:
+                    continue
+
+                # If it's a simple name/value pair, map directly to the value string
+                if 'value' in item and len(item) == 2:
+                    data['envMap'][name] = item['value']
+                else:
+                    # For valueFrom or other complex attributes, map the rest of the dictionary
+                    data['envMap'][name] = {k: v for k, v in item.items() if k != 'name'}
         else:
             data['envMap'] = {}
         del data['extraEnvs']
     return data
 
 def map_to_list(data):
-    """Converts envMap dictionary to extraEnvs list."""
+    """Converts envMap dictionary back to extraEnvs list."""
     if 'envMap' in data:
         if isinstance(data['envMap'], dict):
-            data['extraEnvs'] = [{'name': k, 'value': v} for k, v in data['envMap'].items()]
+            data['extraEnvs'] = []
+            for k, v in data['envMap'].items():
+                item = {'name': k}
+                # If the value is a dictionary (like valueFrom), merge it into the item
+                if isinstance(v, dict):
+                    item.update(v)
+                else:
+                    item['value'] = v
+                data['extraEnvs'].append(item)
         else:
             data['extraEnvs'] = []
         del data['envMap']
@@ -26,7 +45,7 @@ def main():
     parser = argparse.ArgumentParser(description="Convert Helm YAML lists to maps and vice versa.")
     parser.add_argument('-r', '--reverse', action='store_true', help="Convert maps back to lists")
     parser.add_argument('input_file', help="Path to the input YAML file")
-    
+
     args = parser.parse_args()
 
     try:
